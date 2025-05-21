cf930f0d5d51f61b6c04edbe52bd1f9cb19f0c77
Semyon1104 <sema_titov_04@mail.ru>
2025-05-21 20:26:18 +0300

    fix cy selectors

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,3 +1,5 @@
+import { SELECTORS } from '../support/selectors';
+
 describe('Burger Constructor', () => {
   beforeEach(() => {
     cy.intercept('GET', '**/ingredients', { fixture: 'ingredients.json' }).as('getIngredients');
@@ -6,50 +8,50 @@ describe('Burger Constructor', () => {
     cy.wait('@getIngredients', { timeout: 10000 });
     cy.wait('@getUser', { timeout: 10000 });
     // Проверяем, что приложение загрузилось
-    cy.get('[data-testid="burger-constructor"]', { timeout: 10000 }).should('exist');
+    cy.get(SELECTORS.BURGER_CONSTRUCTOR, { timeout: 10000 }).should('exist');
   });
 
   it('should add ingredient to constructor', () => {
-    cy.get('[data-testid="ingredient-item"]').first().trigger('dragstart');
-    cy.get('[data-testid="burger-constructor"]').trigger('drop');
-    cy.get('[data-testid="burger-constructor"]').should('contain', 'Выберите начинку');
+    cy.get(SELECTORS.INGREDIENT_ITEM).first().trigger('dragstart');
+    cy.get(SELECTORS.BURGER_CONSTRUCTOR).trigger('drop');
+    cy.get(SELECTORS.BURGER_CONSTRUCTOR).should('contain', 'Выберите начинку');
   });
 
   it('should open ingredient modal', () => {
-    cy.get('[data-testid="ingredient-item"]').first().click();
-    cy.get('[data-testid="modal"]').should('be.visible');
+    cy.get(SELECTORS.INGREDIENT_ITEM).first().click();
+    cy.get(SELECTORS.MODAL).should('be.visible');
   });
 
   it('should close modal when clicking overlay', () => {
-    cy.get('[data-testid="ingredient-item"]').first().click();
-    cy.get('[data-testid="modal"]').should('be.visible');
-    cy.get('[data-testid="modal-overlay"]').click({ force: true });
-    cy.get('[data-testid="modal"]').should('not.exist');
+    cy.get(SELECTORS.INGREDIENT_ITEM).first().click();
+    cy.get(SELECTORS.MODAL).should('be.visible');
+    cy.get(SELECTORS.MODAL_OVERLAY).click({ force: true });
+    cy.get(SELECTORS.MODAL).should('not.exist');
   });
 
   it('should create order', () => {
     // Проверяем, что пользователь авторизован
-    cy.get('[data-testid="burger-constructor"]').should('exist');
+    cy.get(SELECTORS.BURGER_CONSTRUCTOR).should('exist');
     
     // Добавляем булку
-    cy.get('[data-testid="ingredient-item"]').first().find('button').click();
-    cy.get('[data-testid="burger-constructor"]').should('not.contain', 'Выберите булки');
+    cy.get(SELECTORS.INGREDIENT_ITEM).first().find('button').click();
+    cy.get(SELECTORS.BURGER_CONSTRUCTOR).should('not.contain', 'Выберите булки');
 
     // Добавляем начинку
-    cy.get('[data-testid="ingredient-item"]').eq(1).find('button').click();
-    cy.get('[data-testid="burger-constructor"]').should('not.contain', 'Выберите начинку');
+    cy.get(SELECTORS.INGREDIENT_ITEM).eq(1).find('button').click();
+    cy.get(SELECTORS.BURGER_CONSTRUCTOR).should('not.contain', 'Выберите начинку');
 
     // Мокаем запрос на создание заказа
     cy.intercept('POST', '**/orders', { fixture: 'order.json' }).as('createOrder');
 
     // Проверяем, что кнопка создания заказа активна
-    cy.get('[data-testid="order-button"]').should('be.visible').and('not.be.disabled').click();
+    cy.get(SELECTORS.ORDER_BUTTON).should('be.visible').and('not.be.disabled').click();
     
     // Ждем ответа от сервера
     cy.wait('@createOrder');
     
     // Проверяем, что модальное окно с номером заказа открылось
-    cy.get('[data-testid="modal"]').should('be.visible');
-    cy.get('[data-testid="modal"]').should('contain', '12345');
+    cy.get(SELECTORS.MODAL).should('be.visible');
+    cy.get(SELECTORS.MODAL).should('contain', '12345');
   });
 }); 
---
+++ cypress/support/selectors.ts
@@ -0,0 +1,7 @@
+export const SELECTORS = {
+  INGREDIENT_ITEM: '[data-testid="ingredient-item"]',
+  BURGER_CONSTRUCTOR: '[data-testid="burger-constructor"]',
+  MODAL: '[data-testid="modal"]',
+  MODAL_OVERLAY: '[data-testid="modal-overlay"]',
+  ORDER_BUTTON: '[data-testid="order-button"]'
+} as const; 
